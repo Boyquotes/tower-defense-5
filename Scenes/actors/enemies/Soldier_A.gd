f18515a4a36b03774c941dc8dd3767c3c6e28ccb
Quollveth <77359018+Quollveth@users.noreply.github.com>
2023-05-16 16:57:18 -0300

    despawn properly

--- Scenes/actors/enemies/Soldier_A.gd
+++ Scenes/actors/enemies/Soldier_A.gd
@@ -5,4 +5,4 @@ extends CharacterBody2D
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
-		queue_free()
+		get_parent().queue_free()
