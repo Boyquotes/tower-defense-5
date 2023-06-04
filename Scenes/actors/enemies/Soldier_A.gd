cdc44f11336b54c15696d46def51b1d8a0347aaf
Quollveth <77359018+Quollveth@users.noreply.github.com>
2023-06-04 15:05:45 -0300

    aaaaa

--- Scenes/actors/enemies/Soldier_A.gd
+++ Scenes/actors/enemies/Soldier_A.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 500
+@export var health = 10
 
 func _ready():
 	add_to_group("enemies")
@@ -9,3 +10,8 @@ func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		get_parent().queue_free()
+
+func _damage(damage):
+	health = health-damage
+	if health <= 0:
+		queue_free()
--- Scenes/actors/turrets/bullet_sm.gd
+++ Scenes/actors/turrets/bullet_sm.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-var direction := Vector2.RIGHT
+@export var direction := Vector2.RIGHT
 var speed := 400
+@export var damage = 1
 
 func _ready():
 	set_as_top_level(true)
@@ -11,10 +12,11 @@ func _ready():
 func _physics_process(delta):
 	var v = direction * speed * delta
 	var c := move_and_collide(v)
-	#if c and c.collider:
-		#do shit here
-	#	queue_free()
-	pass
+	if c and c.get_collider():
+		var collider = c.get_collider()
+		if collider.is_in_group("enemies"):
+			collider._damage(damage)
+			queue_free()
 	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
