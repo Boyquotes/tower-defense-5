98d3cf65ca3ad1d21513da2ff7081580b604ed65
Quollveth <77359018+Quollveth@users.noreply.github.com>
2023-06-05 21:21:47 -0300

    can aim again
    some would say this is important

--- Scenes/actors/turrets/gun_t1.gd
+++ Scenes/actors/turrets/gun_t1.gd
@@ -1,4 +1,4 @@
 extends "res://Scenes/actors/turrets/gun_turrets.gd"
 
-func _ready():
-	initialize()
+#hello i exist
+#do not use _ready and _physics_process functions or they'll overwrite the ones from the parent class
--- Scenes/actors/turrets/gun_turrets.gd
+++ Scenes/actors/turrets/gun_turrets.gd
@@ -7,6 +7,7 @@ extends Node2D
 @export var fire_rate = 1.0 #shots per second
 @export var damage = 10
 @export var pierce = 3 #how many enemies it can hit with each shot
+var _projectile_scene = preload("res://Scenes/actors/turrets/bullet_sm.tscn")
 
 #scene tree variables, parts of the turret to be loaded by each scene based on which turret
 #all turrets have a immobile base that has a area2D to serve as the range detection
@@ -20,7 +21,12 @@ extends Node2D
 @onready var _muzzles = []
 @onready var _timer = $Timer
 
-func initialize(): #why wont _ready function run from outside >:(
+#instance variables
+var currTargets = []
+var target = null
+var can_fire := true
+
+func _ready():
 	var i = 1
 	while true:
 		var currMuzz := get_node_or_null("gun/muzzle"+str(i))
@@ -34,12 +40,44 @@ func initialize(): #why wont _ready function run from outside >:(
 	_range.body_shape_exited.connect(body_exit)
 	_timer.wait_time = 1/fire_rate
 
-func test():
-	for muzzle in _muzzles:
-		print("pew pew "+str(muzzle))
+func _physics_process(delta):	
+	if target != null:
+		aim(delta)
 
 func body_enter(body_rid, body, body_shape_index, local_shape_inde):
-	print("hello " + str(body))
+	#get all bodies in range and add to targets array if they are enemies
+	for bodies in get_node("base/range").get_overlapping_bodies():
+		if bodies.is_in_group("enemies"):
+			if !currTargets.has(bodies):
+				currTargets.append(bodies)			
+			#if no current target set first body in array as target
+			if target == null:
+				target = currTargets[0]	
 
 func body_exit(body_rid, body, body_shape_index, local_shape_index):
-	print("bye " + str(body))
+	if body == target:
+		if currTargets.has(body):
+			currTargets.remove_at(currTargets.find(body))
+			target = null
+
+func aim(phy_delta):
+	#vector from turret to ship and it's angle
+	var v =target.global_position - global_position
+	var angle = v.angle()
+	#rotation allowed per frame
+	var angle_delta = rotaion_speed * phy_delta
+	#get full rotation and limit it to allowed rotation
+	var r = _gun_node.get_rotation()
+	angle = lerp_angle(r,angle,1.0)
+	angle = clamp(angle, r - angle_delta, r + angle_delta)
+	_gun_node.set_rotation(angle)
+
+func shoot():
+	var projectile = _projectile_scene.instantiate()
+	projectile.add_collision_exception_with(self)
+	projectile.damage = damage
+	#projectile.transform = muzzle.global_transform
+	add_child(projectile)
+	
+func on_timer_timeout():
+	can_fire = true
